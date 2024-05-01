4bacdbc0f0f1fdb04a8fafa6b43e02d52a481914
ZegarkOOO <kondzioklarnet007@gmail.com>
2024-05-01 23:48:19 +0200

    Update Fluke8808A
    cały koncept i prototyp połaczenia i łączenia sie z urządzeniem. Podobnie trzeba zrobić do innych urządzeń np it_is() oraz wygląd całej klasy jak wygląda i jak funkcjonuje

--- FLUKE8808A.py
+++ FLUKE8808A.py
@@ -1,2 +1,42 @@
-class FLUKE8808A:
-    a = 0
+import pyvisa
+import time
+
+class Fluke_8808A:
+
+
+    def __init__(self, port, baud_rate, mode = 1, timeout=500):
+        self.instr = pyvisa.ResourceManager().open_resource(port)
+        self.mode = mode #tryb działania multimetru (only info)
+        self.instr.baud_rate = baud_rate
+        self.instr.timeout = timeout
+        #ustawić format odbiranych danych na notację naukową
+
+    async def start_measure(self, duration, gather_freq ,file):
+        #zrobić z HOLD i można odczytywać co jakiś ustalony czas ESSA
+        pass
+
+    def reset(self):
+        self.instr.write('*RST')
+        time.sleep(14)
+        self.instr.clear()
+
+    def it_is(self):
+        answer = ""
+        self.instr.clear()
+        try:
+            self.instr.read()
+            answer = self.instr.query("*IDN?")
+        except Exception as err:
+            print(f"ERROR Unexpected {err=}, {type(err)=}")
+        return answer
+
+        
+
+#rm = pyvisa.ResourceManager()
+#print(rm.list_resources())
+#visa_device = rm.open_resource('ASRL4::INSTR')
+
+stm = Fluke_8808A('ASRL4::INSTR', 19200)
+
+#stm.reset()
+print(stm.it_is())
