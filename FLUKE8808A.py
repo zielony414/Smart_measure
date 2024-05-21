980354c60483595dfd95c924753c08396e843c73
ZegarkOOO <kondzioklarnet007@gmail.com>
2024-05-21 19:28:19 +0200

    Potężne zmiany
    Dodałem zabezpieczenia z połaczeniem z kilkoma portami na raz, i brakiem wystarczającej ilości urządzeń do startu.
    Dodałem okienka wyskakujące: błąd, ostrzeżenie, info. Klasa w osobnym pliku Small_window.py
    Zmiany w start_measurements multimetrów, zwracają średnią z wyników i zapisują do pliku

--- FLUKE8808A.py
+++ FLUKE8808A.py
@@ -2,13 +2,15 @@ import pyvisa
 import asyncio
 import time
 import datetime
-
+import Small_window as sm
+import re
 
 class Fluke_8808A:
 
     '''The constructor sets all the most important items'''
     def  __init__(self, port, baud_rate=19200, mode=1, timeout=5000):
         self.instr = pyvisa.ResourceManager().open_resource(port)
+        self.port = port
         self.mode = mode  # multimetr operation mode (only info)
         self.instr.baud_rate = baud_rate
         self.instr.timeout = timeout
@@ -34,6 +36,17 @@ class Fluke_8808A:
         self.instr.write("AUTO\n")
         time.sleep(1)
 
+    def extract_number(self, text):
+        # Wyrażenie regularne do wyszukania liczby w notacji wykładniczej
+        match = re.search(r'([+-]?\d*\.?\d+([eE][+-]?\d+)?)', text)
+        if match:
+            return match.group(0)
+        else:
+            raise ValueError("No number found in the text")
+
+    def convert_scientific_to_float(self, sci_num):
+        return float(sci_num)
+
     '''The main method for starting measurements with time specification'''
     async def start_measure(self, duration, gather_freq, file):
         start_time = time.time()
@@ -50,7 +63,7 @@ class Fluke_8808A:
             self.instr.read()
             self.instr.read()
 
-            # 
+
             time_captured = datetime.datetime.now().strftime("%X.%f")[:-4]
             string = str(time_captured + " " + reader)
 
@@ -63,14 +76,14 @@ class Fluke_8808A:
     
     '''The main method for starting measurements with number of measurments'''
     async def start_measure2(self, number_of_measurements: int, file):
-        start_time = time.time()
-
+        #start_time = time.time()
+        measurement_list = []
         # clearing the communication buffer
         self.instr.clear()
         gather_freq = 1
-        duration = number_of_measurements * gather_freq;
+        #duration = number_of_measurements * gather_freq;
         # a while loop that runs for a specified period of time
-        while time.time() - start_time < duration:
+        for i in range(number_of_measurements):
 
             # VERY IMPORTANT section retrieves results from the multimeter
             # the reading is performed 3 times because the device still sends prompts ("=>")
@@ -78,15 +91,23 @@ class Fluke_8808A:
             self.instr.read()
             self.instr.read()
 
+            measurement_list.append(reader)
             
-            time_captured = datetime.datetime.now().strftime("%X.%f")[:-4]
-            string = str(time_captured + " " + reader)
+            #time.sleep(gather_freq)
+            await asyncio.sleep(gather_freq)
+        
+        # sum of those measurmments 
+        sum = 0.00
+        for number in measurement_list:
+            sci_number = self.extract_number(number)
+            float_number = self.convert_scientific_to_float(sci_number)
+            sum += float_number
+        time_captured = datetime.datetime.now().strftime("%X.%f")[:-4]
+        string = str(time_captured + " " + str(float(sum/number_of_measurements)))
 
-            print(string)
-            file.write(string)
-            
-            time.sleep(gather_freq)
-            #await asyncio.sleep(gather_freq)
+        print(string)
+        file.write(string)
+
         return 1
 
     '''The method resets the measuring device'''
@@ -102,7 +123,8 @@ class Fluke_8808A:
         try:
             answer = self.instr.query("*IDN?")
         except Exception as err:
-            print(f"ERROR Unexpected {err=}, {type(err)=}")
+            news = sm.Small_window()
+            news.show_error(f"ERROR {self.port} Fluke8808A Unexpected {err=}, {type(err)=}")
         return answer
 
     '''A method that sets the multimeter into voltmeter mode'''
@@ -116,6 +138,4 @@ class Fluke_8808A:
         self.instr.write("ADC\n")
         time.sleep(5)
         return 1
-
-
 
---
+++ Small_window.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMessageBox
+
+class Small_window():
+
+    def show_error(self, text):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setWindowTitle("Error")
+        msg.setText("An error occured: " + text)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
+
+    def show_warning(self, text):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setWindowTitle("Warning")
+        msg.setText("It is warning: " + text)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
+
+    def show_info(self, text):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowTitle("Information")
+        msg.setText(text)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
